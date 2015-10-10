29b007e9323df7e5ca897db09ea901337e906025
DimasBro0110 <dimasgitacc@gmail.com>
2015-10-11 01:58:35 +0200

    ПРограмма Поиска Анаграмм. Можно использовать запустив экзешник, можно через cmd, передав в качестве 1-го агрумента файл в котором слова, 2-го агрумента - выходной файл с разбитыми по группам анаграммами.

---
+++ AnagrammFinder/AnagrammFinder/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace AnagrammFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                Table tab = new Table(args[0], args[1]);
+                tab.downloadToFile();
+                Menu();
+            }
+            else
+            {
+                Menu();
+            }
+        }
+
+        public static void Menu()
+        {
+            string req = "", f_name_1 = "", f_name_2 = "";
+            do
+            {
+                Console.WriteLine("1. Ввести вх/вых файлы");
+                Console.WriteLine("0. Выход");
+                Console.Write(">> ");
+                req = Console.ReadLine();
+
+                switch (req)
+                {
+                    case "1":
+                        Console.WriteLine("Введите путь к входному файлу >> ");
+                        f_name_1 = Console.ReadLine();
+                        Console.WriteLine("Введите путь к выходному файлу >> ");
+                        f_name_2 = Console.ReadLine();
+                        if (f_name_1.Length != 0 || f_name_2.Length != 0)
+                        {
+                            Table table = new Table(f_name_1, f_name_2);
+                            table.downloadToFile();
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Были введены неккоректные данные, повторите ввод");
+                            break;
+                        }
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Были введены неккоректные данные, повторите ввод");
+                        break;
+                }
+
+            }
+            while (req != "0");
+        }
+    }
+
+}
---
+++ AnagrammFinder/AnagrammFinder/Table.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace AnagrammFinder
+{
+    class Table
+    {
+        private Dictionary<int, LinkedList<string>> table =
+                new Dictionary<int, LinkedList<string>>();
+
+        private string file_in_name;
+        private string file_out_name;
+
+        private void setter()
+        {
+            Encoding enc = Encoding.GetEncoding(1251);
+            StreamReader sr = null;
+
+            try
+            {
+                sr = new StreamReader(this.file_in_name, enc);
+                string line = "";
+                int sum = 1;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    foreach (char ch in line.ToLower())
+                    {
+                        sum *= (int)ch;
+                    }
+                    this.setValues(sum, line);
+                    sum = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Возможно был введен неверный путь к файлу / имени файла");
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Dispose();
+                    sr.Close();
+                }
+            }
+        }
+
+        private void setValues(int hash, string word)
+        {
+            if (this.table.ContainsKey(hash))
+            {
+                this.table[hash].AddLast(word);
+            }
+            else
+            {
+                LinkedList<string> lst = new LinkedList<string>();
+                lst.AddLast(word);
+                this.table.Add(hash, lst);
+            }
+        }
+
+        public Table(string in_name, string out_name)
+        {
+            this.file_in_name = in_name;
+            this.file_out_name = out_name;
+            this.setter();
+        }
+
+        public void downloadToFile()
+        {
+            if (this.table.Count != 0)
+            {
+                Encoding enc = Encoding.GetEncoding(1251);
+                StreamWriter sw = null;
+                try
+                {
+                    sw = new StreamWriter(this.file_out_name, false, enc);
+                    foreach (KeyValuePair<int, LinkedList<string>> cur in this.table)
+                    {
+                        if (cur.Value.Count() > 1)
+                        {
+                            foreach (string word in cur.Value)
+                            {
+                                sw.Write(word + " ");
+                            }
+                            sw.WriteLine();
+                        }
+                    }
+                    Console.WriteLine("Данные были выгружены, смотрите {0} файл", this.file_out_name);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Возможно был введен неверный путь к файлу / имени файла");
+                }
+                finally
+                {
+                    if (sw != null)
+                    {
+                        sw.Dispose();
+                        sw.Close();
+                    }
+                }
+            }
+        }
+
+        ~Table()
+        {
+            foreach (KeyValuePair<int, LinkedList<string>> kv in this.table)
+            {
+                kv.Value.Clear();
+            }
+            this.table.Clear();
+        }
+    }
+
+}
